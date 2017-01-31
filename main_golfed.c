736bfddc7497391fc4e4437007eb17802ce8f5fe
Mark Calhoun <markcalhoun62@gmail.com>
2017-01-31 10:13:21 -0600

    changed getchar to getch to save 4 bytes

--- main_golfed.c
+++ main_golfed.c
@@ -23,14 +23,13 @@ if(es[i].h<=0){++c->g;A(EY(i),EX(i),'.');es[i].h=5;EX(i)=rand()%27+2;EY(i)=rand(
 ;_C ch;initscr();noecho();curs_set(FALSE);F(0,yb,i)F(0,xb,j)A(i,j,'.');F(0,yb,i)A(i,0,'#');F(0,yb,i
 )A(i,xb-1,'#');F(0,xb,i)A(0,i,'#');F(3,6,i)A(5,i,'#');F(2,5,i)A(i,5,'#');F(0,xb,i)A(yb-1,i,'#');CT;
 X=1;Y=1;PH=10;ch.g=0;UH(ch.h,ch.g);F(0,3,i){es[i].h=5;EX(i)=rand()%27+2;EY(i)=rand()%7+2;A(EY(i),EX
-(i),'Y');}A(Y,X,'@');refresh();c=getchar();while(c!='Q'){CT;A(Y,X,'.');if(c=='w'){if(CE(Y-1,X)){H(Y
--1,X,es,&ch);}EI(CW(Y-1,X)){WH;AH;}else{--Y;AH;}}EI(c=='a'){if(CE(Y,X-1)){H(Y,X-1,es,&ch);}EI(CW(Y,
-X-1)){WH;AH;}else{--X;AH;}}EI(c=='s'){if(CE(Y+1,X)){H(Y+1,X,es,&ch);}EI(CW(Y+1,X)){WH;AH;}else{++Y;
-AH;}}EI(c=='d'){if(CE(Y,X+1)){H(Y,X+1,es,&ch);}EI(CW(Y,X+1)){WH;AH;}else{++X;AH;}}EI(c=='.'){AH;}F(
-0,3,i){A(EY(i),EX(i),'.');if(((X==EX(i)-1||X==EX(i)+1)&&Y==EY(i))||((Y==EY(i)-1||Y==EY(i)+1)&&X==EX
-(i))){--PH;}else{if(X>EX(i)){if(CE(EY(i),EX(i)+1)!=true&&CW(EY(i),EX(i)+1)!=true){++EX(i);}}EI(X<EX
-(i)){if(CE(EY(i),EX(i)-1)!=true&&CW(EY(i),EX(i)-1)!=true){--EX(i);}}EI(Y>EY(i)){if(CE(EY(i)+1,EX(i))
-!=true&&CW(EY(i)+1,EX(i))!=true){++EY(i);}}EI(Y<EY(i)){if(CE(EY(i)-1,EX(i))!=true&&CW(EY(i)-1,EX(i))
-!=true){--EY(i);}}}A(EY(i),EX(i),'Y');}if(PH<=0){PS(5,3,"You Loose! Press Q");getch();break;}if(ch.g
->=20){PS(5,3,"You Win! Press Q");getch();break;}A(Y,X,'@');UH(PH,ch.g);refresh();c=getchar();}endwin
-();return 0;}
+(i),'Y');}A(Y,X,'@');refresh();c=getch();while(c!='Q'){CT;A(Y,X,'.');if(c=='w'){if(CE(Y-1,X)){H(Y-1
+,X,es,&ch);}EI(CW(Y-1,X)){WH;AH;}else{--Y;AH;}}EI(c=='a'){if(CE(Y,X-1)){H(Y,X-1,es,&ch);}EI(CW(Y,X-
+1)){WH;AH;}else{--X;AH;}}EI(c=='s'){if(CE(Y+1,X)){H(Y+1,X,es,&ch);}EI(CW(Y+1,X)){WH;AH;}else{++Y;AH
+;}}EI(c=='d'){if(CE(Y,X+1)){H(Y,X+1,es,&ch);}EI(CW(Y,X+1)){WH;AH;}else{++X;AH;}}EI(c=='.'){AH;}F(0,
+3,i){A(EY(i),EX(i),'.');if(((X==EX(i)-1||X==EX(i)+1)&&Y==EY(i))||((Y==EY(i)-1||Y==EY(i)+1)&&X==EX(i
+))){--PH;}else{if(X>EX(i)){if(CE(EY(i),EX(i)+1)!=true&&CW(EY(i),EX(i)+1)!=true){++EX(i);}}EI(X<EX(i
+)){if(CE(EY(i),EX(i)-1)!=true&&CW(EY(i),EX(i)-1)!=true){--EX(i);}}EI(Y>EY(i)){if(CE(EY(i)+1,EX(i))!
+=true&&CW(EY(i)+1,EX(i))!=true){++EY(i);}}EI(Y<EY(i)){if(CE(EY(i)-1,EX(i))!=true&&CW(EY(i)-1,EX(i))
+!=true){--EY(i);}}}A(EY(i),EX(i),'Y');}if(PH<=0){PS(5,3,"You Loose!");getch();break;}if(ch.g>=20){
+PS(5,3,"You Win!");getch();break;}A(Y,X,'@');UH(PH,ch.g);refresh();c=getch();}endwin();return 0;}
